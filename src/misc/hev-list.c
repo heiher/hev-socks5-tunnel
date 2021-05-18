1cf3a91f67fd2a4ec2b40ffa8cce54d67c8a383e
hev <r@hev.cc>
2021-05-18 22:22:13 +0800

    HevList: Init.

---
+++ src/misc/hev-list.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : hev-list.c
+ Authors     : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2019 everyone.
+ Description : Double-linked List
+ ============================================================================
+ */
+
+#include <stddef.h>
+
+#include "hev-list.h"
+
+void
+hev_list_add_tail (HevList *self, HevListNode *new_)
+{
+    new_->prev = self->tail;
+    new_->next = NULL;
+
+    if (self->tail)
+        self->tail->next = new_;
+    else
+        self->head = new_;
+    self->tail = new_;
+}
+
+void
+hev_list_del (HevList *self, HevListNode *node)
+{
+    if (node->prev)
+        node->prev->next = node->next;
+    else
+        self->head = node->next;
+
+    if (node->next)
+        node->next->prev = node->prev;
+    else
+        self->tail = node->prev;
+}
---
+++ src/misc/hev-list.h
@@ -0,0 +1,63 @@
+/*
+ ============================================================================
+ Name        : hev-list.h
+ Authors     : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2019 everyone.
+ Description : Double-linked List
+ ============================================================================
+ */
+
+#ifndef __HEV_LIST_H__
+#define __HEV_LIST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct _HevList HevList;
+typedef struct _HevListNode HevListNode;
+
+struct _HevList
+{
+    HevListNode *head;
+    HevListNode *tail;
+};
+
+struct _HevListNode
+{
+    HevListNode *next;
+    HevListNode *prev;
+};
+
+static inline HevListNode *
+hev_list_node_prev (HevListNode *node)
+{
+    return node->prev;
+}
+
+static inline HevListNode *
+hev_list_node_next (HevListNode *node)
+{
+    return node->next;
+}
+
+static inline HevListNode *
+hev_list_first (HevList *self)
+{
+    return self->head;
+}
+
+static inline HevListNode *
+hev_list_last (HevList *self)
+{
+    return self->tail;
+}
+
+void hev_list_add_tail (HevList *self, HevListNode *new_);
+void hev_list_del (HevList *self, HevListNode *node);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __HEV_LIST_H__ */
