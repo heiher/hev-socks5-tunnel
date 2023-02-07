ea42ff1c8f5bd846a71ba09cb1095c6aea1c0007
hev <r@hev.cc>
2023-02-07 12:07:38 +0800

    HevTunnelLinux: Skip first iovec.

--- src/hev-tunnel-linux.h
+++ src/hev-tunnel-linux.h
@@ -21,7 +21,7 @@ static inline ssize_t
 hev_tunnel_readv (int fd, struct iovec *iov, int iovcnt,
                   HevTaskIOYielder yielder, void *yielder_data)
 {
-    return hev_task_io_readv (fd, iov, iovcnt, yielder, yielder_data);
+    return hev_task_io_readv (fd, &iov[1], iovcnt - 1, yielder, yielder_data);
 }
 
 static inline ssize_t
@@ -35,7 +35,7 @@ static inline ssize_t
 hev_tunnel_writev (int fd, struct iovec *iov, int iovcnt,
                    HevTaskIOYielder yielder, void *yielder_data)
 {
-    return hev_task_io_writev (fd, iov, iovcnt, yielder, yielder_data);
+    return hev_task_io_writev (fd, &iov[1], iovcnt - 1, yielder, yielder_data);
 }
 
 #endif /* __HEV_TUNNEL_LINUX_H__ */
