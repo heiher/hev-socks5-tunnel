c495ec774179ccf84136c934c53169948170d39e
jolinhe <jolinhe@tencent.com>
2023-09-21 18:49:34 +0800

    output xcframework

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ buildStatic()
 
      local OUTPUT_ARCH_FILE="libhev-socks5-tunnel-$1-$3.a"
 
-     libtool -static -o libhev-socks5-tunnel-$1-$3.a \
+     libtool -static -o $OUTPUT_ARCH_FILE \
                    bin/libhev-socks5-tunnel.a \
                    third-part/lwip/bin/liblwip.a \
                    third-part/yaml/bin/libyaml.a \
@@ -20,22 +20,31 @@ buildStatic()
 }
 
 buildStatic iphoneos -mios-version-min=16.0 arm64
-buildStatic iphonesimulator -mios-version-min=16.0 x86_64
-buildStatic iphonesimulator -mios-version-min=16.0 arm64
+buildStatic iphonesimulator -miphonesimulator-version-min=16.0 x86_64
+buildStatic iphonesimulator -miphonesimulator-version-min=16.0 arm64
 buildStatic macosx -mmacosx-version-min=13.3 x86_64
 buildStatic macosx -mmacosx-version-min=13.3 arm64
 
-libtool -static -o libhev-socks5-tunnel-ios.a \
-               libhev-socks5-tunnel-iphoneos-arm64.a \
-               libhev-socks5-tunnel-iphonesimulator-x86_64.a \
-               libhev-socks5-tunnel-iphonesimulator-arm64.a
+lipo -create \
+	-arch x86_64 libhev-socks5-tunnel-iphonesimulator-x86_64.a \
+	-arch arm64  libhev-socks5-tunnel-iphonesimulator-arm64.a \
+	-output libhev-socks5-tunnel-iphonesimulator.a
 
-libtool -static -o libhev-socks5-tunnel-macosx.a \
-               libhev-socks5-tunnel-macosx-arm64.a \
-               libhev-socks5-tunnel-macosx-x86_64.a
+lipo -create \
+	-arch x86_64 libhev-socks5-tunnel-macosx-x86_64.a \
+	-arch arm64 libhev-socks5-tunnel-macosx-arm64.a \
+	-output libhev-socks5-tunnel-macosx.a
 
+xcodebuild -create-xcframework \
+    -library libhev-socks5-tunnel-iphoneos-arm64.a -headers include \
+    -library libhev-socks5-tunnel-macosx.a -headers include \
+    -library libhev-socks5-tunnel-iphonesimulator.a -headers include \
+    -output libhev-socks5-tunnel.xcframework
+
+rm -rf libhev-socks5-tunnel-iphoneos-arm64.a
 rm -rf libhev-socks5-tunnel-macosx-arm64.a
 rm -rf libhev-socks5-tunnel-macosx-x86_64.a
-rm -rf libhev-socks5-tunnel-iphoneos-arm64.a
+rm -rf libhev-socks5-tunnel-macosx.a
 rm -rf libhev-socks5-tunnel-iphonesimulator-x86_64.a
 rm -rf libhev-socks5-tunnel-iphonesimulator-arm64.a
+rm -rf libhev-socks5-tunnel-iphonesimulator.a
