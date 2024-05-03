6836b560c968de3da357592901d231cb59d01ee0
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-05-03 15:50:08 +0800

    rename to HevSocks5Tunnel

---
+++ apple/HevSocks5Tunnel/HevSocks5Tunnel.h
@@ -0,0 +1,15 @@
+//
+//  HevSocks5Tunnel.h
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HevSocks5Tunnel.
+FOUNDATION_EXPORT double hevSocks5TunnelVersionNumber;
+
+//! Project version string for HevSocks5Tunnel.
+FOUNDATION_EXPORT const unsigned char hevSocks5TunnelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HevSocks5Tunnel/PublicHeader.h>
+
+#import <HevSocks5Tunnel/hev-main.h>
