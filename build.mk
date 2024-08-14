a9f8599595b04b9663cc597121dad6120333211b
hev <r@hev.cc>
2024-08-14 21:04:09 +0800

    Build: Read rev-id from file.

--- build.mk
+++ build.mk
@@ -5,4 +5,16 @@ rwildcard=$(foreach d,$(wildcard $1*), \
           $(filter $(subst *,%,$2),$d))
 
 SRCFILES=$(call rwildcard,$(SRCDIR)/,*.c *.S)
-VERSION_CFLAGS=-DCOMMIT_ID=\"$(shell git -C $(SRCDIR) rev-parse --short HEAD)\"
+
+ifeq ($(REV_ID),)
+  ifneq (,$(wildcard .rev-id))
+    REV_ID=$(shell cat .rev-id)
+  endif
+  ifeq ($(REV_ID),)
+    REV_ID=$(shell git -C $(SRCDIR) rev-parse --short HEAD)
+  endif
+  ifeq ($(REV_ID),)
+    REV_ID=unknown
+  endif
+endif
+VERSION_CFLAGS=-DCOMMIT_ID=\"$(REV_ID)\"
