75ce74afa752cbfa4ed3337b96dc7f74f717bdbb
hev <git@hev.cc>
2025-10-06 11:11:01 +0800

    Android: Enable all supported ABIs.

--- Application.mk
+++ Application.mk
@@ -15,7 +15,7 @@
 
 APP_OPTIM := release
 APP_PLATFORM := android-29
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_CFLAGS := -O3
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 NDK_TOOLCHAIN_VERSION := clang
